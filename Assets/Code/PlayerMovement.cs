2cbe651ac5d1a1d0c69bd6d7b822e9e9c19ba98e
Shugo0016 <93568520+Shugo0016@users.noreply.github.com>
2022-10-14 11:32:47 -0700

    Wall Jumping Attempt

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -6,17 +6,23 @@ using TMPro;
 public class PlayerMovement : MonoBehaviour
 {
     public TextMeshProUGUI StarText;
+    public Rigidbody2D theRB;
     public Transform feetTrans;
     public LayerMask groundLayer;
+    public Transform wallGrabPoint;
 
     public int speed = 5;
     public int jumpForce = 300;
     public int airjumps = 1;
+    private float gravityStore;
+    public float wallJumpTime = .2f;
+    private float wallJumpCounter;
 
     Rigidbody2D _rigidbody;
 
     public bool grounded = false;
     public bool facingRight = true;
+    private bool canGrab, isGrabbing;
     float xSpeed = 0;
 
     public GameObject Stars;
@@ -25,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        gravityStore = theRB.gravityScale;  
     }
 
     void FixedUpdate()
@@ -43,42 +50,82 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        grounded = Physics2D.OverlapCircle(feetTrans.position, .1f, groundLayer);
+        if(wallJumpCounter <= 0)
+        {
+            grounded = Physics2D.OverlapCircle(feetTrans.position, .1f, groundLayer);
 
-        if (grounded)
-        {
-            airjumps = 1;
-            if (Input.GetButtonDown("Jump"))
+            if (grounded)
             {
-                _rigidbody.AddForce(new Vector2(0, jumpForce));
-            }
-        }
-        else if (airjumps > 0)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                _rigidbody.AddForce(new Vector2(0, jumpForce));
-                airjumps--;
-            }
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            if (PublicVars.stars != 0)
-            {
-                PublicVars.stars -= 1;
-                StarText.text = "STARS: " + PublicVars.stars;
-                GameObject collectedStar = Instantiate(Stars, transform.position, transform.rotation);
-                if (facingRight)
+                airjumps = 1;
+                if (Input.GetButtonDown("Jump"))
                 {
-                    collectedStar.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 1) * starForce);
-                }
-                else
-                {
-                    collectedStar.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 1) * starForce);
+                    _rigidbody.AddForce(new Vector2(0, jumpForce));
                 }
             }
+            else if (airjumps > 0)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    _rigidbody.AddForce(new Vector2(0, jumpForce));
+                    airjumps--;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (PublicVars.stars != 0)
+                {
+                    PublicVars.stars -= 1;
+                    StarText.text = "STARS: " + PublicVars.stars;
+                    GameObject collectedStar = Instantiate(Stars, transform.position, transform.rotation);
+                    if (facingRight)
+                    {
+                        collectedStar.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 1) * starForce);
+                    }
+                    else
+                    {
+                        collectedStar.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 1) * starForce);
+                    }
+                }
+            }
+
+            //Handle Wall Jumping
+            canGrab = Physics2D.OverlapCircle(wallGrabPoint.position, 0.2f, groundLayer);
+
+            isGrabbing = false;
+            if (canGrab && !grounded)
+            {
+                if ((transform.localScale.x == 1f && Input.GetAxisRaw("Horizontal") > 0) || (transform.localScale.x == -1f && Input.GetAxisRaw("Horizontal") < 0))
+                {
+                    isGrabbing = true;
+                }
+
+            }
+
+
+            if (isGrabbing)
+            {
+                theRB.gravityScale = 0f;
+                theRB.velocity = Vector2.zero;
+
+                if (Input.GetButtonDown("Jump")) {
+                    wallJumpCounter = wallJumpTime;
+                    theRB.velocity = new Vector2(-Input.GetAxisRaw("Horizontal") * speed, jumpForce);
+                    theRB.gravityScale = gravityStore;
+                    isGrabbing = false;
+                }
+            }
+            else
+            {
+                theRB.gravityScale = gravityStore;
+            }
         }
+        else
+        {
+            wallJumpCounter -= Time.deltaTime;
+        }
+
+ 
     }
 
     void Flip()
