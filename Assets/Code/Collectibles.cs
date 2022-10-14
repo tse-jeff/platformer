8fb01a86d1c16985e23637f688b2fe8866e16b35
stiztics <il2149@nyu.edu>
2022-10-14 15:31:30 -0400

    Fixed star counter

--- Assets/Code/Collectibles.cs
+++ Assets/Code/Collectibles.cs
@@ -8,6 +8,11 @@ public class Collectibles : MonoBehaviour
     public TextMeshProUGUI Score;
     public TextMeshProUGUI Stars;
     // Start is called before the first frame update
+    void Start() {
+        Stars.text = "STARS: " + PublicVars.stars;
+        Score.text = "SCORE: " + PublicVars.score;
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("NinjaStar"))
