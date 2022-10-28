b0449442d72d9dcb369488e8340aa8fe3e4215c3
AdityaDheer <ad4961@nyu.edu>
2022-10-28 02:01:49 -0400

    level2 difficulty fix

--- Assets/Code/PlayerCombat.cs
+++ Assets/Code/PlayerCombat.cs
@@ -45,7 +45,8 @@ public class PlayerCombat : MonoBehaviour
         // death
         if (PublicVars.playerHealth < 1)
         {
-            if(gameObject.tag != "Dead"){
+            if (gameObject.tag != "Dead")
+            {
                 PublicVars.lives--;
                 _audioSource.PlayOneShot(deathSound, volume);
                 gameObject.tag = "Dead";
@@ -130,7 +131,8 @@ public class PlayerCombat : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
 
-        if(PublicVars.lives >= 0){
+        if (PublicVars.lives >= 0)
+        {
             //Reload Scene
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
@@ -139,7 +141,8 @@ public class PlayerCombat : MonoBehaviour
             PublicVars.stars = PublicVars.prevStars;
             PublicVars.score = PublicVars.prevScore;
         }
-        else{
+        else
+        {
             SceneManager.LoadScene("Game Over");
         }
 
